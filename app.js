9a4b9ae5052e324ec5ac0502d433774b2753e3be
Nooorrrr <noor.bnhdd@gmail.com>
2025-02-08 09:18:48 +0100

    atp i wanna kms

--- app.js
+++ app.js
@@ -1,64 +1,41 @@
-const createError = require("http-errors");
-const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-// Environment Variables
-require("dotenv").config();
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-// Database
-const db = require("./models");
+var app = express();
 
-// Routers
-const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-// Initialize Express App
-const app = express();
-
-// View Engine Setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
-
-// Middleware
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Routes
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
-// Database Connection & Synchronization
-sequelize
-  .sync({ alter: true })
-  .then(() => {
-    console.log("✅ Database synchronized");
-  })
-  .catch((err) => {
-    console.error("❌ Failed to sync database:", err);
-  });
-
-// Catch 404 and Forward to Error Handler
-app.use((req, res, next) => {
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// Error Handler
-app.use((err, req, res, next) => {
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-  res.status(err.status || 500);
-  res.render("error");
-});
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-// Start Server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`🚀 Server is running on port ${PORT}`);
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 module.exports = app;
